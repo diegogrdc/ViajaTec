91c5769592b6caf8f2ef83d6a5131be38f95bde8
Citlalli27 <70440578+Citlalli27@users.noreply.github.com>
2022-10-03 20:20:25 -0500

    Login diseño

---
+++ src/components/loginform.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./loginform.css" 
+
+const LoginForm = () => {
+    return (
+        <div className="page">
+            <div className="cover">
+            <h1>Viaja-Tec</h1>
+            <input type="text" placeholder="Matricula" />
+            <input type="password" placeholder="Contraseña" />
+            
+            <div className="login-btn">Ingresar</div>
+
+
+        </div>
+
+        </div>
+      
+    )
+}
+
+export default LoginForm
