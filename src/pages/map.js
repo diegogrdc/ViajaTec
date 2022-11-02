1b4da39a6a2dd35187f8523f19d2ac1e3eb3f88b
Diego García <diegomysterio@gmail.com>
2022-11-02 17:29:04 -0600

    Add map and carousel

--- src/pages/map.js
+++ src/pages/map.js
@@ -1,6 +1,11 @@
 import React, { useRef } from "react";
-import { useGoogleMap, useMap } from "../hooks/map_hook.js";
-const API_KEY = "AIzaSyBcKg0tiyLBZ4Rvk4PlZR_V64ndFZOHbfs";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import RutaGarzaSada from '../assets/RutaGarzaSada.png'
+import RutaValleAlto from '../assets/RutaValleAlto.png'
+import RutaEscuelasYHospitales from '../assets/RutaEscuelasYHospitales.png'
+import RutaNocturna from '../assets/RutaNocturna.png'
+import RutaRevolucion from '../assets/RutaRevolucion.png'
 
 const initialConfig = {
   zoom: 12,
@@ -8,17 +13,42 @@ const initialConfig = {
 };
 
 const Gmap = () => {
-  const googleMap = useGoogleMap(API_KEY);
-  const mapContainerRef = useRef(null);
-  useMap({ googleMap, mapContainerRef, initialConfig });
+
   return (
-    <div
-      style={{
-        height: "100vh",
-        width: "100%"
-      }}
-      ref={mapContainerRef}
-    />
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      <div style={{padding: "20px", width: "800px", height: "600px"}}>
+        <iframe src="https://www.google.com/maps/d/embed?mid=1n_C1lCcAobnXuwPc-H9h3qoWQ3PcXls&ehbc=2E312F" width="100%" height="100%">
+        </iframe>
+      </div>
+      <div style={{padding: "20px"}}>
+        <h1>Rutas y Horarios</h1>
+        <p>Servicio invierno, enero-mayo, verano y agosto-diciembre, 2022</p>
+        <div style={{width: "500px", height:"400px"}}>
+        <Carousel>
+              <div>
+                  <img src={RutaGarzaSada} />
+                  <p className="legend">Ruta Garza Sada</p>
+              </div>
+              <div>
+                  <img src={RutaEscuelasYHospitales} />
+                  <p className="legend">Ruta Escuelas Y Hospitales</p>
+              </div>
+              <div>
+                  <img src={RutaRevolucion} />
+                  <p className="legend">Ruta Revolucion</p>
+              </div>
+              <div>
+                  <img src={RutaValleAlto} />
+                  <p className="legend">Ruta Valle Alto</p>
+              </div>
+              <div>
+                  <img src={RutaNocturna} />
+                  <p className="legend">Rutas Nocturnas</p>
+              </div>
+          </Carousel>
+          </div>
+      </div>
+    </div>
   );
 };
 
