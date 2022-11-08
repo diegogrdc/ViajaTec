cd84f67b37e333adfccf875a2fe996972f2ee919
Citlalli27 <70440578+Citlalli27@users.noreply.github.com>
2022-11-08 09:32:51 -0600

    Diseño Reglas

--- src/pages/rules.js
+++ src/pages/rules.js
@@ -1,25 +1,100 @@
 import React from 'react'
+import "./rules.css"
 
 function Rules() {
   return (
-    <div>
-        <h2><span>Recomendaciones Importantes</span></h2>
-        <ul>
-            <li><strong>Utiliza el cubrebocas en todo momento sin retirarlo.</strong></li>
-            <li><strong>Utiliza el equipo de protección personal (EPP) de manera correcta y durante todo el recorrido.</strong></li>
-            <li><strong>Respeta la sana distancia (1.5 mts.).</strong></li>
-            <li><strong>Permite la toma de temperatura.</strong></li>
-            <li><strong>Aplícate gel antibacterial (proporcionado dentro de la unidad).</strong></li>
-            <li>Revisa siempre tus pertenencias antes de descender de la unidad de servicio para evitar inconvenientes.</li>
-            <li>Por tu seguridad, al caminar en la vía pública evita el uso de accesorios que necesiten tu atención, como teléfonos celulares, reproductores de música, entre otros.</li>
-            <li>Sé puntual al abordar la unidad y llega con 10 minutos de anticipación a tu parada.</li>
-            <li>Evita abordar la unidad a mediación de la calle, fuera de la banqueta o en doble fila.</li>
-            <li>Siempre presenta tu credencial con el IDentificador vigente al operador.</li>
-            <li>Toma un solo asiento, ve correctamente sentado durante todo el viaje y abrocha tu cinturón de seguridad.</li>
-            <li>Mantén un comportamiento que no incomode a los usuarios ni distraiga al operador.</li>
-            <li>Desciende solamente en las paradas oficiales y hasta que la unidad realice el alto total.</li>
-        </ul>
-        <p>Los presentes lineamientos son para el conocimiento y la observancia de los usuarios <strong>(estudiantes, profesores y colaboradores)</strong>. El área de Movilidad podrá intervenir en algunos casos no contemplados y/o mencionados, de acuerdo con los Compromisos de Integridad del Tecnológico de Monterrey.</p>
+    <div className='container'>
+      <br></br>
+      <br></br>
+      <h1>Recomendaciones Importantes</h1>
+      <br></br>
+      <br></br>
+      <p className='para'>Los presentes lineamientos son para el conocimiento y la observancia de los usuarios (estudiantes, profesores y colaboradores). 
+        El área de Movilidad podrá intervenir en algunos casos no contemplados y/o mencionados, de acuerdo con los Compromisos de Integridad del Tecnológico de Monterrey.</p>
+      <div className='container2'>
+      <div className='box'>
+        <h2>01</h2>
+        <p>Utiliza el cubrebocas en todo momento sin retirarlo.</p>
+      </div>
+
+      <div className='box'>
+        <h2>02</h2>
+        <p>Utiliza el equipo de protección personal (EPP) de manera correcta y durante todo el recorrido.</p>
+      </div>
+
+      <div className='box'>
+        <h2>03</h2>
+        <p>Respeta la sana distancia (1.5 mts.).</p>
+      </div>
+
+      <div className='box'>
+        <h2>04</h2>
+        <p>Permite la toma de temperatura.</p>
+      </div>
+
+      <div className='box'>
+        <h2>05</h2>
+        <p>Aplícate gel antibacterial (proporcionado dentro de la unidad).</p>
+      </div>
+
+      <br></br>
+
+      <div className='box'>
+        <h2>06</h2>
+        <p>Revisa siempre tus pertenencias antes de descender de la unidad de servicio para evitar inconvenientes.</p>
+      </div>
+
+
+      <div className='box'>
+        <h2>07</h2>
+        <p>Por tu seguridad, al caminar en la vía pública evita el uso de accesorios que necesiten tu atención, 
+          como teléfonos celulares, reproductores de música, entre otros.</p>
+      </div>
+
+      <div className='box'>
+        <h2>08</h2>
+        <p>Sé puntual al abordar la unidad y llega con 10 minutos de anticipación a tu parada.</p>
+      </div>
+
+
+      <div className='box'>
+        <h2>09</h2>
+        <p>Evita abordar la unidad a mediación de la calle, fuera de la banqueta o en doble fila.</p>
+      </div>
+
+      <div className='box'>
+        <h2>10</h2>
+        <p>Siempre presenta tu credencial con el IDentificador vigente al operador.</p>
+      </div>
+
+      <div className='box'>
+        <h2>11</h2>
+        <p>Toma un solo asiento, ve correctamente sentado durante todo el viaje y abrocha tu cinturón de seguridad.</p>
+      </div>
+
+
+      <div className='box'>
+        <h2>12</h2>
+        <p>Mantén un comportamiento que no incomode a los usuarios ni distraiga al operador.</p>
+      </div>
+
+      <div className='box'>
+        <h2>13</h2>
+        <p>Desciende solamente en las paradas oficiales y hasta que la unidad realice el alto total.</p>
+      </div>
+
+      <div className='box'>
+        <h2>14</h2>
+        <p>Para utilizar el servicio, todos los usuarios deben presentar al operador su credencial vigente del Tecnológico de Monterrey.</p>
+      </div>
+
+      <div className='box'>
+        <h2>15</h2>
+        <p>La unidad no recogerá usuarios que esperen en lugares no autorizados en los croquis oficiales de las rutas.</p>
+      </div>
+
+
+      </div>
     </div>
   )
 }
